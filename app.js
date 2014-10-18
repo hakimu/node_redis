d8acc6dd1a4b50cee89209464f011a519c2a7eef
Hakimu Jackson <hakimu@newrelic.com>
2014-10-18 16:06:58 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+var newrelic = require('newrelic');
+var express = require('express');
+
+var app = express();
+
+var redis = require('redis');
+var client = redis.createClient();
+
+app.get('/', function(req,res){
+	res.send('This is the home page');
+});
+
+app.get('/redis', function(req,res){
+	setTimeout(function(){console.log('Whatup!');},5000);
+	client.on('error', function(err){
+		console.log("Error" + err);
+	});
+
+	client.set("string key", "Hello World", redis.print);
+	client.get("string key", function(err,reply){
+		res.send(reply.toString());
+	});
+	client.quit();
+});
+
+app.listen(8888);
+console.log('Let the games begin...on port 8888');
